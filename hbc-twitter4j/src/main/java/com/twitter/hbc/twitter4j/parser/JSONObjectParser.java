f97e9644904d874de5c40c9220b765cc53e3aedb
Kevin Oliver <koliver@twitter.com>
2013-02-28 11:26:12 -0800

    hackityhack

--- hbc-twitter4j/src/main/java/com/twitter/hbc/twitter4j/parser/JSONObjectParser.java
+++ hbc-twitter4j/src/main/java/com/twitter/hbc/twitter4j/parser/JSONObjectParser.java
@@ -22,10 +22,35 @@ import twitter4j.internal.org.json.JSONObject;
 
 public class JSONObjectParser {
 
+//  public static class CompatibleStatusDeletionNotice implements StatusDeletionNotice {
+//    private final long statusId;
+//    private final long userId;
+//    CompatibleStatusDeletionNotice(long statusId, long userId) {
+//      this.statusId = statusId;
+//      this.userId = userId;
+//    }
+//
+//    @Override
+//    public long getStatusId() {
+//      return statusId;
+//    }
+//
+//    @Override
+//    public long getUserId() {
+//      return userId;
+//    }
+//
+//    // @Override
+//    public int compareTo(Object o) {
+//      return Longs.compare(statusId, ((StatusDeletionNotice)o).getStatusId());
+//    }
+//  }
+
   public static StatusDeletionNotice parseStatusDelete(JSONObject json) throws JSONException {
     JSONObject statusDelete = json.getJSONObject("delete").getJSONObject("status");
     final long statusId = statusDelete.getLong("id");
     final long userId = statusDelete.getLong("user_id");
+    //return new CompatibleStatusDeletionNotice(statusId, userId);
     return new StatusDeletionNotice() {
       @Override
       public long getStatusId() {
@@ -37,9 +62,9 @@ public class JSONObjectParser {
         return userId;
       }
 
-      @Override
-      public int compareTo(StatusDeletionNotice statusDeletionNotice) {
-        return Longs.compare(getStatusId(), statusDeletionNotice.getStatusId());
+      // @Override lol java 7's javac
+      public int compareTo(Object o) {
+        return Longs.compare(getStatusId(), ((StatusDeletionNotice)o).getStatusId());
       }
     };
   }
